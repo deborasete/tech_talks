0939ca67e2edab3516773454d0198d173e1a57d6
Débora Sete Acosta <sete.debora@gmail.com>
2025-02-13 14:38:28 -0300

    updates content and images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Feb 12, 2025 19:00:00");
+const dataDoEvento = new Date("Feb 28, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaPeriodo = setInterval(function() {
